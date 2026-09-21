b5dab2344d0764a65e139acd33f3115874f69a3f
aelamal <elamal.a980@ucd.ac.ma>
2026-09-21 11:05:45 +0000

    modifer method convesrt in JwtConverter avec Oauth2

--- src/main/java/com/example/securitykeycloak/security/JwtConverter.java
+++ src/main/java/com/example/securitykeycloak/security/JwtConverter.java
@@ -1,12 +1,45 @@
 package com.example.securitykeycloak.security;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
+@Configuration
+public class JwtConverter implements Converter<Jwt, JwtAuthenticationToken> {
 
-public class JwtConverter {
-    @Bean
-    public JwtConverter jwtConverter() {
-        return new JwtConverter();
+    private final JwtGrantedAuthoritiesConverter defaultAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    @Override
+    public JwtAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                defaultAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()
+        ).collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(jwt, authorities, jwt.getClaimAsString("preferred_username"));
+    }
+
+    private Collection<GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        if (realmAccess == null || realmAccess.get("roles") == null) {
+            return Set.of();
+        }
+
+        Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
     }
 }
--- src/main/java/com/example/securitykeycloak/security/SecurityConfigue.java
+++ src/main/java/com/example/securitykeycloak/security/SecurityConfigue.java
@@ -1,28 +1,42 @@
 package com.example.securitykeycloak.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigue {
 
+    private final JwtConverter jwtConverter;
+    public SecurityConfigue(JwtConverter jwtConverter) {
+        this.jwtConverter = jwtConverter;
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/v1/hello/user").hasRole("USER")
+                        .requestMatchers("/api/v1/hello/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-                .logout(LogoutConfigurer::permitAll);
+
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter))
+                );
+
         return http.build();
     }
 
+
+    // commit : ajoute method convert jwt
+    // khassni nrigl ligne 26
+    // create user , admin password 1234
 }
